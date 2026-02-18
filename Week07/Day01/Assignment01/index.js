b7c9e37da059c986102b29a3f0f5cd468d514772
Rashid Ahmadi <rashid.angury20@gmail.com>
2026-02-18 23:02:32 +0100

    Week 07 day 01 assignment 01 added

---
+++ Week07/Day01/Assignment01/index.js
@@ -0,0 +1,14 @@
+function test(){
+    var a="3";
+    var b="8";
+
+    // Do not change the code above
+    // Write your code in line 7 to 9
+
+
+
+    // Do not change the code below
+
+    console.log("a is "+a);
+    console.log("b is "+b);
+}
