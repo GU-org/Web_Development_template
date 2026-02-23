af045b2eb877b2be1bf09da93f2d89702004763d
Rashid Ahmadi <rashid.angury20@gmail.com>
2026-02-23 12:11:36 +0100

    Week 07 day 01 assignment 02 added

--- Week07/Day01/Assignment02/index.js
+++ Week07/Day01/Assignment02/index.js
@@ -1,14 +1,11 @@
-function test(){
-    var a="3";
-    var b="8";
+//Challenge: changing casing in Text:
+//1.	Write a JavaScript program to transform a user's input name such that the first letter of the name is converted to uppercase, while the rest of the letters remain as they are. This transformation improves the formatting of names entered in various scenarios like forms, user interfaces, or databases.
+//2.	If the name is all in uppercase, remain the first character as Uppercase and change the rest to lowercase. 
 
-    // Do not change the code above
-    // Write your code in line 7 to 9
+var yourName=prompt("Please enter your name !");
+//write you code below
 
 
 
-    // Do not change the code below
 
-    console.log("a is "+a);
-    console.log("b is "+b);
-}
+alert("Welcome "+yourName+" to my new software");
