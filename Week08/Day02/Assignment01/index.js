eee80b5bdd2eed108b746a84fa0d6aa23f823ee3
Rashid Ahmadi <rashid.angury20@gmail.com>
2026-03-05 13:04:16 +0100

    Week09 day01 assignment01 corrected

--- Week08/Day02/Assignment01/index.js
+++ Week08/Day02/Assignment01/index.js
@@ -1,4 +1,17 @@
 function bmiCalc(w,h){
-    bmi=w/(h*h);
-    return bmi;
+    let bmi=(w/Math.pow(h,2));
+    return Math.round(bmi);
+}
+
+let wight=prompt("Please enter your wight in KG");
+let hight=prompt("Please enter your hight in m");
+let bmi=bmiCalc(wight,hight);
+if(bmi<18.5){
+    alert("Your BMI is "+bmi+" so you are underweight.")
+}
+else if(bmi<24.9){
+    alert("Your BMI is "+bmi+" so you have a normal weight.");
+}
+else {
+    alert("Your BMI is "+bmi+", so you are overweight.");
 }
--- Week08/Day02/Assignment02/index.js
+++ Week08/Day02/Assignment02/index.js
@@ -3,10 +3,26 @@ function isLeap(year) {
 /**************Don't change the code above****************/    
     
     //Write your code here.    
+if(year%4===0){
+    if(year%100===0){
+        if(year%400===0){
+            alert(year+" is a leap year");
+        }
+    } else {
+        alert(year +" is not a leap year");
+    }
+    
 
+}
+else {
+    alert(year+" is not a leap year");
+}
 
     
 
 /**************Don't change the code below****************/    
 
 }
+
+let year=prompt("Pleaes enter a year");
+isLeap(year);
